49652000433b222a8cc131c3aba47b1f7125a5e6
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-12-14 07:10:30 +0000

    p61まで終了 タイトルとサブタイトルを指定しスプレット構文を使い直接コンポーネントに渡した

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Hero from 'components/hero'
-
+const props1 = { title: 'about', subtitle: 'about development activities' }
 const About = () => {
-  return <Hero />
+  return <Hero {...props1} />
 }
 
 export default About
