746b4f5f5054425199c44be1286110da3f472af1
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2023-12-20 04:55:30 +0000

    chapter4.4終了 footerに.wrapperを適用し、背景色を指定した。

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,14 @@
 import Logo from 'components/logo'
-const Footer = () => (
-  <footer>
-    <Logo />
-  </footer>
-)
+import styles from 'styles/footer.module.css'
+const Footer = () => {
+  return (
+    <footer className={styles.wrapper}>
+      <div className={styles.flexContainer}>
+        <Logo />
+        [ソーシャル]
+      </div>
+    </footer>
+  )
+}
 
 export default Footer
