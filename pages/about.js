e0070d21d949c3467f1c71afcfb5171cccad2ca4
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-01-07 15:06:27 +0000

    chapter6.3終了 <Head>を<meta>に書き換え、metaコンポーネントでかんりできるようにした。

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
@@ -14,9 +14,7 @@ const props1 = { title: 'about', subtitle: 'about development activities' }
 const About = () => {
   return (
     <Container>
-      <Head>
-        <title>アバウト</title>
-      </Head>
+      <Meta pageTitle='アバウト' />
       <Hero {...props1} />
 
       <figure>
