f0117011f3e618bd9f3b8688aa51f0f374427623
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-01-16 10:25:49 +0000

    error修正

--- components/post-categories.js
+++ components/post-categories.js
@@ -1,13 +1,13 @@
 import styles from 'styles/post-categories.module.css'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFolder0pen } from '@fortawesome/free-regular-svg-icons'
+import { faFolderOpen } from '@fortawesome/free-regular-svg-icons'
 
 const PostCategories = ({ categories }) => {
   return (
     <div className={styles.flexContainer}>
       <h3 className={styles.heading}>
-        <FontAwesomeIcon icon={faFolder0pen} />
+        <FontAwesomeIcon icon={faFolderOpen} />
         <span className='sr-only'>Categories</span>
       </h3>
       <ul className={styles.list}>
