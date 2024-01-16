845d0eb8176951bafeb6a4ecf7ec69812fa5c24d
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-01-16 08:11:41 +0000

    chapter9.7終了 paginationコンポーネントでトップページに記事一覧を表示させた。

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,46 @@
+import { getAllPosts } from 'lib/api'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Meta from 'components/meta'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+
+import { eyecatchLocal } from 'lib/constants'
+
 const props = {
   title: 'CUBE',
   subtitle: 'アウトプットしていくサイト',
-  imageOn: []
+  imageOn: true
 }
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <Container>
       <Meta />
       <Hero {...props} />
+
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
     </Container>
   )
 }
+
+const getStaticProps = async () => {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!Object.prototype.hasOwnProperty.call(post, 'eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
 export default Home
+export { getStaticProps }
