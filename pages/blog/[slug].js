d8575d31a71418a8a0160df0d30be82de83a4edd
Shimabukuro Sora <s23012@std.it-college.ac.jp>
2024-01-18 06:53:27 +0000

    chapter9 AllSlugsで5件に記事のスラッグを取得し、最初の5件だけが生成されるようにした。

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -77,35 +77,39 @@ const Post = ({
 }
 
 const getStaticPaths = async () => {
-  const allSlugs = await getAllSlugs()
+  const allSlugs = await getAllSlugs(5)
 
   return {
     paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
 const getStaticProps = async context => {
   const slug = context.params.slug
   const post = await getPostBySlug(slug)
-  const description = extractText(post.content)
-  const eyecatch = post.eyecatch ?? eyecatchLocal
+  if (!post) {
+    return { notFound: true }
+  } else {
+    const description = extractText(post.content)
+    const eyecatch = post.eyecatch ?? eyecatchLocal
 
-  const { base64 } = await getPlaiceholder(eyecatch.url)
-  eyecatch.blurDataURL = base64
+    const { base64 } = await getPlaiceholder(eyecatch.url)
+    eyecatch.blurDataURL = base64
 
-  const allSlugs = await getAllSlugs()
-  const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
-  return {
-    props: {
-      title: post.title,
-      publish: post.publishDate,
-      content: post.content,
-      eyecatch,
-      categories: post.categories,
-      description,
-      prevPost,
-      nextPost
+    const allSlugs = await getAllSlugs()
+    const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
+    return {
+      props: {
+        title: post.title,
+        publish: post.publishDate,
+        content: post.content,
+        eyecatch,
+        categories: post.categories,
+        description,
+        prevPost,
+        nextPost
+      }
     }
   }
 }
